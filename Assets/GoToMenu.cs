82d2ea3b0d9ef62af8b08336429410a2c9fb9098
AlexMercer2000 <anwesh2000@rediffmail.com>
2019-02-11 19:31:22 +0530

    Survival game created using Unity3D

---
+++ Assets/GoToMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMenu : MonoBehaviour {
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -2);
+    }
+
+}
